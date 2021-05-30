54055712987f16ea6943f0b80812a4d42dde693b
Parthose25 <84158326+Parthose25@users.noreply.github.com>
2021-05-31 04:15:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const https = require("https");
+const bodyParser = require("body-parser");
+const app = express();
+app.set("view engine","ejs" );
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req,res){
+res.render("home");
+});
+
+app.post("/",function(req,res){
+    const query = req.body.cityName;
+        const units = "metric";
+        const url = (`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${process.env.APIKEY}&units=${units}`);
+
+        https.get(url,function(response){
+
+        response.on("data",function(data){
+            const weatherData =  JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const desc = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const img = "https://openweathermap.org/img/wn/" + icon +"@2x.png";
+            res.render("weather",{desc:desc,temp:temp,img:img,query:query});
+           
+        });
+        }); 
+});
+
+        
+app.listen(3000, function(){
+    console.log("Server is running on the port 3000");
+});
